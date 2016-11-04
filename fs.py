12ad78d2d918274b72bf387f27f6cf637c0b8c3e
Adam Palmar <s144868@student.dtu.dk>
2016-11-04 16:09:59 +0100

    added neural network

--- fs.py
+++ fs.py
@@ -13,6 +13,7 @@ from scipy.io import loadmat
 import sklearn.linear_model as lm
 from sklearn import cross_validation
 import pandas as pd
+import neurolab as nl
 from toolbox_02450 import feature_selector_lr, bmplot
 
 # Load data from matlab file
@@ -22,18 +23,25 @@ from toolbox_02450 import feature_selector_lr, bmplot
 #attributeNames = [name[0] for name in mat_data['attributeNames'][0]]
 
 
-data = pd.read_csv('C:/Users/Thor/Desktop/5.semester/Swedish-Forrest-Fire/CleanSet.csv')
+data = pd.read_csv('cleanSet.csv')
 #print mat_data
 #X = np.matrix(mat_data['X'])
 #y = np.matrix(mat_data['y'], dtype=int)
 #X = np.matrix(mat_data.drop('Cases',axis=1).dropna())
 #y = np.matrix(mat_data['Cases'].apply(f)).T
-data_normalized = (data - data.mean()) / (data.std())
-X = np.matrix(data_normalized.drop(['reportedCrime','reportedCrimeVandalism','Cases', 'medianIncome'],axis=1).dropna())
+#data_normalized = (data - data.mean()) / (data.std())
+#X = np.matrix(data_normalized.drop(['reportedCrime','reportedCrimeVandalism'],axis=1).dropna())
+
+data_normalized =  (data - data.mean()) / (data.max() - data.min())
+X = np.matrix(data_normalized.drop(['reportedCrime','reportedCrimeVandalism'],axis=1).dropna())
 # y = np.matrix((data['Cases'] / data['population']) *1e3)
 y = np.matrix(data['reportedCrime']).T
 
-attributeNames = data_normalized.drop(['reportedCrime','reportedCrimeVandalism','Cases', 'medianIncome'],axis=1).dropna().columns.values
+
+
+
+
+attributeNames = data_normalized.drop(['reportedCrime','reportedCrimeVandalism'],axis=1).dropna().columns.values
 N, M = X.shape
 
 # Add offset attribute
@@ -41,9 +49,20 @@ X = np.concatenate((np.ones((X.shape[0],1)),X),1)
 #attributeNames = [u'Offset']+attributeNames
 M = M+1
 
+#Neural network parameters
+list_input_range = [[-1,1] for i in range(M)]
+# Parameters for neural network classifier
+n_hidden_units = 40      # number of hidden units
+n_train = 2             # number of networks trained in each k-fold
+
+# These parameters are usually adjusted to: (1) data specifics, (2) computational constraints
+learning_goal = 200     # stop criterion 1 (train mse to be reached)
+max_epochs = 1000       # stop criterion 2 (max epochs in training)
+
+
 ## Crossvalidation
 # Create crossvalidation partition for evaluation
-K = 10
+K = 2
 CV = cross_validation.KFold(N,K,shuffle=True)
 
 # Initialize variables
@@ -54,7 +73,10 @@ Error_train_fs = np.empty((K,1))
 Error_test_fs = np.empty((K,1))
 Error_train_nofeatures = np.empty((K,1))
 Error_test_nofeatures = np.empty((K,1))
-
+Error_train_nn = np.empty((K,1))
+Error_test_nn = np.empty((K,1))
+Error_list_fs = list()
+Error_list_nn = list()
 k=0
 for train_index, test_index in CV:
     
@@ -81,7 +103,14 @@ for train_index, test_index in CV:
     m = lm.LinearRegression().fit(X_train[:,selected_features], y_train)
     Error_train_fs[k] = np.square(y_train-m.predict(X_train[:,selected_features])).sum()/y_train.shape[0]
     Error_test_fs[k] = np.square(y_test-m.predict(X_test[:,selected_features])).sum()/y_test.shape[0]
-
+    Error_list_fs.append(abs(y_test-m.predict(X_test[:,selected_features])))
+    #Compute neural network
+    ann = nl.net.newff(list_input_range, [n_hidden_units, 1], [nl.trans.TanSig(),nl.trans.PureLin()])
+        # train network 
+    list_of_training_errors = ann.train(X_train, y_train, goal=learning_goal, epochs=max_epochs, show=round(100))    
+    Error_train_nn[k] = list_of_training_errors[-1]  
+    Error_test_nn[k] = np.square(y_test-ann.sim(X_test)).sum()/y_test.shape[0]
+    Error_list_nn.append(abs(y_test - ann.sim(X_test)))
     figure(k)
     subplot(1,2,1)
     plot(range(1,len(loss_record)), loss_record[1:])
@@ -117,6 +146,12 @@ print('- Test error:     {0}'.format(np.sqrt(Error_test_fs).mean()))
 print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_fs.sum())/Error_train_nofeatures.sum()))
 print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test_fs.sum())/Error_test_nofeatures.sum()))
 
+
+# Use T-test to check if classifiers are significantly different
+
+
+
+
 figure(k)
 subplot(1,3,2)
 bmplot(attributeNames, range(1,Features.shape[1]+1), -Features)
@@ -143,4 +178,32 @@ for i in range(0,len(ff)):
    xlabel(attributeNames[ff[i]])
    ylabel('residual error')
 
-show()    
+show() 
+
+t_test_fs = Error_list_fs[-1]
+t_test_nn = Error_list_nn[-1]
+   
+t_test_average = abs(y_test - np.mean(y_train))
+
+
+[tstatistic, pvalue] = stats.ttest_ind(t_test_fs,t_test_nn)
+if pvalue<=0.05:
+    print('Classifiers are significantly different. (p={0})'.format(pvalue[0]))
+else:
+    print('Classifiers are not significantly different (p={0})'.format(pvalue[0]))        
+
+[tstatistic, pvalue] = stats.ttest_ind(t_test_fs,t_test_average)
+if pvalue<=0.05:
+    print('Classifiers are significantly different. (p={0})'.format(pvalue[0]))
+else:
+    print('Classifiers are not significantly different (p={0})'.format(pvalue[0]))        
+    
+# Boxplot to compare classifier error distributions
+figure()
+boxplot(np.bmat('t_test_fs, t_test_nn,t_test_average'))
+xlabel('Linear Regression FS  vs.   Neural net')
+ylabel('Cross-validation error [%]')
+
+show()
+   
+   
