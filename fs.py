a3ad44fb6a5b6c0b35cff97850c8c33121a4b5e5
Dzyans <tadahlstrom@gmail.com>
2016-11-07 14:20:32 +0100

    now improved

--- fs.py
+++ fs.py
@@ -2,7 +2,9 @@
 """
 Created on Fri Nov 04 13:10:45 2016
 
-@author: Thor
+@author: Adam Palmer, Thor Dahsltrøm
+
+Based on the framework provided by -Det er vigtigt at skive___ 
 """
 
 ##crossvalidation and forward feature selection on regression for reported crime
@@ -12,8 +14,10 @@ from pylab import *
 from scipy.io import loadmat
 import sklearn.linear_model as lm
 from sklearn import cross_validation
+from sklearn import feature_selection as skfs
 import pandas as pd
 import neurolab as nl
+from scipy import stats
 from toolbox_02450 import feature_selector_lr, bmplot
 
 # Load data from matlab file
@@ -31,14 +35,11 @@ data = pd.read_csv('cleanSet.csv')
 #y = np.matrix(mat_data['Cases'].apply(f)).T
 #data_normalized = (data - data.mean()) / (data.std())
 #X = np.matrix(data_normalized.drop(['reportedCrime','reportedCrimeVandalism'],axis=1).dropna())
-
+data['youngVsOld'] = (data['populationShare65plus'] / data['youngUnskilled'] )
 data_normalized =  (data - data.mean()) / (data.max() - data.min())
-X = np.matrix(data_normalized.drop(['reportedCrime','reportedCrimeVandalism'],axis=1).dropna())
+X = np.matrix(data_normalized.drop(['reportedCrime','reportedCrimeVandalism','urbanDegree'],axis=1).dropna())
 # y = np.matrix((data['Cases'] / data['population']) *1e3)
 y = np.matrix(data['reportedCrime']).T
-
-
-
 
 
 attributeNames = data_normalized.drop(['reportedCrime','reportedCrimeVandalism'],axis=1).dropna().columns.values
@@ -56,13 +57,13 @@ n_hidden_units = 40      # number of hidden units
 n_train = 2             # number of networks trained in each k-fold
 
 # These parameters are usually adjusted to: (1) data specifics, (2) computational constraints
-learning_goal = 200     # stop criterion 1 (train mse to be reached)
+learning_goal = 180 # stop criterion 1 (train mse to be reached)
 max_epochs = 1000       # stop criterion 2 (max epochs in training)
 
 
 ## Crossvalidation
 # Create crossvalidation partition for evaluation
-K = 2
+K = 4
 CV = cross_validation.KFold(N,K,shuffle=True)
 
 # Initialize variables
@@ -97,20 +98,26 @@ for train_index, test_index in CV:
     Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
 
     # Compute squared error with feature subset selection
+    #print skfs.f_regression(X_train, y_train, True)
     selected_features, features_record, loss_record = feature_selector_lr(X_train, y_train, internal_cross_validation)
+    #print selected_features
+    #Sprint features_record
     Features[selected_features,k]=1
     # .. alternatively you could use module sklearn.feature_selection
+    
+    
     m = lm.LinearRegression().fit(X_train[:,selected_features], y_train)
     Error_train_fs[k] = np.square(y_train-m.predict(X_train[:,selected_features])).sum()/y_train.shape[0]
     Error_test_fs[k] = np.square(y_test-m.predict(X_test[:,selected_features])).sum()/y_test.shape[0]
     Error_list_fs.append(abs(y_test-m.predict(X_test[:,selected_features])))
     #Compute neural network
     ann = nl.net.newff(list_input_range, [n_hidden_units, 1], [nl.trans.TanSig(),nl.trans.PureLin()])
-        # train network 
+    # train network 
     list_of_training_errors = ann.train(X_train, y_train, goal=learning_goal, epochs=max_epochs, show=round(100))    
     Error_train_nn[k] = list_of_training_errors[-1]  
     Error_test_nn[k] = np.square(y_test-ann.sim(X_test)).sum()/y_test.shape[0]
     Error_list_nn.append(abs(y_test - ann.sim(X_test)))
+    
     figure(k)
     subplot(1,2,1)
     plot(range(1,len(loss_record)), loss_record[1:])
@@ -133,19 +140,23 @@ for train_index, test_index in CV:
 # Display results
 print('\n')
 print('Average:\n')
-print('- Training error: {0}'.format(np.sqrt(Error_train_nofeatures).mean() )) 
+print('- Training error: {0}'.format(np.sqrt(Error_train_nofeatures).mean())) 
 print('- Test error:     {0}'.format(np.sqrt(Error_test_nofeatures).mean()))
 print('Linear regression without feature selection:\n')
 print('- Training error: {0}'.format(np.sqrt(Error_train).mean() )) 
 print('- Test error:     {0}'.format(np.sqrt(Error_test).mean()))
 print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
 print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
-print('Linear regression with feature selection:\n')
+print('\n\nLinear regression with feature selection:\n')
 print('- Training error: {0}'.format(np.sqrt(Error_train_fs).mean()))
 print('- Test error:     {0}'.format(np.sqrt(Error_test_fs).mean()))
 print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_fs.sum())/Error_train_nofeatures.sum()))
 print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test_fs.sum())/Error_test_nofeatures.sum()))
-
+print('\n\nArtificial Neural Network:\n')
+print('- Training error: {0}'.format(np.sqrt(Error_train_nn).mean()))
+print('- Test error:     {0}'.format(np.sqrt(Error_test_nn).mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_nn.sum())/Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test_nn.sum())/Error_test_nofeatures.sum()))
 
 # Use T-test to check if classifiers are significantly different
 
@@ -201,7 +212,7 @@ else:
 # Boxplot to compare classifier error distributions
 figure()
 boxplot(np.bmat('t_test_fs, t_test_nn,t_test_average'))
-xlabel('Linear Regression FS  vs.   Neural net')
+xlabel('Linear Regression FS vs. Neural net vs. mean Error')
 ylabel('Cross-validation error [%]')
 
 show()
